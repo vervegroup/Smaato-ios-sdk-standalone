2973bfca17113f9dd2d00cb85bf1c25ed6827031
Aysel Abdullayeva <aysel.abdullayeva@verve.com>
2026-02-06 16:27:44 +0100

    switch to single wrapper commit

--- Package.swift
+++ Package.swift
@@ -6,15 +6,7 @@ let package = Package(
     name: "SmaatoSDKStandalone",
     platforms: [.iOS(.v12)],
     products: [
-        .library(name: "SmaatoSDK", targets: ["SmaatoSDKDependencies"]),
-
-        .library(name: "SmaatoSDKCore", targets: ["SmaatoSDKCoreDependencies"]),
-        .library(name: "SmaatoSDKBanner", targets: ["SmaatoSDKBannerDependencies"]),
-        .library(name: "SmaatoSDKInterstitial", targets: ["SmaatoSDKInterstitialDependencies"]),
-        .library(name: "SmaatoSDKRewardedAds", targets: ["SmaatoSDKRewardedAdsDependencies"]),
-        .library(name: "SmaatoSDKNative", targets: ["SmaatoSDKNativeDependencies"]),
-        .library(name: "SmaatoSDKUnifiedBidding", targets: ["SmaatoSDKUnifiedBiddingDependencies"]),
-        .library(name: "SmaatoSDKInAppBidding", targets: ["SmaatoSDKInAppBiddingDependencies"]),
+        .library(name: "SmaatoSDK", targets: ["SmaatoSDKDependencies"])
     ],
     targets: [
         .binaryTarget(name: "ATOM", path: "ATOM.xcframework"),
@@ -28,7 +20,7 @@ let package = Package(
         .binaryTarget(name: "SmaatoSDKUnifiedBiddingBinary", path: "SmaatoSDKUnifiedBidding.xcframework"),
         .binaryTarget(name: "SmaatoSDKInAppBiddingBinary", path: "SmaatoSDKInAppBidding.xcframework"),
 
-        .binaryTarget(name: "SmaatoHyBidAdapterBinary", path: "Smaato_HyBid.xcframework"),
+        .binaryTarget(name: "SmaatoHyBidBianry", path: "Smaato_HyBid.xcframework"),
         .binaryTarget(name: "OMSDK", path: "OMSDK-1.5.4/OMSDK_Pubnativenet.xcframework"),
 
         .target(
@@ -41,46 +33,11 @@ let package = Package(
                 "SmaatoSDKNativeBinary",
                 "SmaatoSDKUnifiedBiddingBinary",
                 "SmaatoSDKInAppBiddingBinary",
+                "SmaatoHyBidBinary",
                 "ATOM",
                 "NGSDK",
                 "OMSDK"
             ],
-            path: "Sources"
-        ),
-
-        .target(
-            name: "SmaatoSDKCoreDependencies",
-            dependencies: ["SmaatoSDKCoreBinary", "ATOM", "NGSDK", "OMSDK"],
-            path: "Sources"
-        ),
-        .target(
-            name: "SmaatoSDKBannerDependencies",
-            dependencies: ["SmaatoSDKBannerBinary", "SmaatoSDKCoreBinary", "ATOM", "NGSDK", "OMSDK"],
-            path: "Sources"
-        ),
-        .target(
-            name: "SmaatoSDKInterstitialDependencies",
-            dependencies: ["SmaatoSDKInterstitialBinary", "SmaatoSDKCoreBinary", "ATOM", "NGSDK", "OMSDK"],
-            path: "Sources"
-        ),
-        .target(
-            name: "SmaatoSDKRewardedAdsDependencies",
-            dependencies: ["SmaatoSDKRewardedAdsBinary", "SmaatoSDKCoreBinary", "ATOM", "NGSDK", "OMSDK"],
-            path: "Sources"
-        ),
-        .target(
-            name: "SmaatoSDKNativeDependencies",
-            dependencies: ["SmaatoSDKNativeBinary", "SmaatoSDKCoreBinary", "ATOM", "NGSDK", "OMSDK"],
-            path: "Sources"
-        ),
-        .target(
-            name: "SmaatoSDKUnifiedBiddingDependencies",
-            dependencies: ["SmaatoSDKUnifiedBiddingBinary", "SmaatoSDKCoreBinary", "ATOM", "NGSDK", "OMSDK"],
-            path: "Sources"
-        ),
-        .target(
-            name: "SmaatoSDKInAppBiddingDependencies",
-            dependencies: ["SmaatoSDKInAppBiddingBinary", "SmaatoSDKCoreBinary", "ATOM", "NGSDK", "OMSDK"],
             path: "Sources"
         )
     ]
